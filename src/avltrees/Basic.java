5b362262e7e7afe8fd67421de721172daefc5735
Sammsul Hoque Choudhary <sammsulhoq@gmail.com>
2025-03-20 13:05:13 +0530

    Implemented methods for calculating height, balance factor, balance and rotations.

--- src/avltrees/Basic.java
+++ src/avltrees/Basic.java
@@ -3,8 +3,8 @@ package avltrees;
 public class Basic {
     public static void main(String[] args) {
         MyAVLTree myAVLTree = new MyAVLTree();
+        myAVLTree.insert(10);
+        myAVLTree.insert(20);
         myAVLTree.insert(30);
-        myAVLTree.insert(15);
-        myAVLTree.insert(18);
     }
 }
--- src/avltrees/MyAVLTree.java
+++ src/avltrees/MyAVLTree.java
@@ -14,10 +14,6 @@ public class MyAVLTree {
         public String toString() {
             return "Node=" + this.value + ", Height=" + this.height;
         }
-
-        public void setHeight(int height) {
-            this.height = height;
-        }
     }
     private AVLNode root;
 
@@ -25,20 +21,77 @@ public class MyAVLTree {
         root = insert(root, value);
     }
 
-    private AVLNode insert(AVLNode node, int value) {
-        if (node == null)
+    private AVLNode insert(AVLNode root, int value) {
+        if (root == null)
             return new AVLNode(value);
 
-        if (value < node.value)
-            node.leftChild = insert(node.leftChild, value);
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
         else
-            node.rightChild = insert(node.rightChild, value);
+            root.rightChild = insert(root.rightChild, value);
 
-        node.height = Math.max(heightOfNode(node.leftChild), heightOfNode(node.rightChild)) + 1;
-        return node;
+        // calculating height of the current node
+        root.height = calculateHeight(root);
+
+        // checking the balance factor to determine the type of rotation to be applied
+        return balance(root);
+    }
+
+    private int calculateHeight(AVLNode node) {
+        return Math.max(heightOfNode(node.leftChild), heightOfNode(node.rightChild)) + 1;
+    }
+
+    private AVLNode balance(AVLNode root) {
+        if (isLeftHeavy(root)) {
+            if (balanceFactor(root.leftChild) < 0)
+                root.leftChild = rotateLeft(root.leftChild);
+
+            return rotateRight(root);
+        } else if (isRightHeavy(root)) {
+            if (balanceFactor(root.rightChild) > 0)
+                root.rightChild = rotateRight(root.rightChild);
+
+            return rotateLeft(root);
+        }
+
+        return root;
+    }
+
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightChild;
+        root.rightChild =  newRoot.leftChild;
+        newRoot.leftChild = root;
+
+        root.height = calculateHeight(root);
+        newRoot.height = calculateHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftChild;
+        root.leftChild = newRoot.rightChild;
+        newRoot.rightChild = root;
+
+        root.height = calculateHeight(root);
+        newRoot.height = calculateHeight(newRoot);
+
+        return newRoot;
     }
 
     private int heightOfNode(AVLNode node) {
         return node == null ? -1 : node.height;
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return (node == null) ? 0 : heightOfNode(node.leftChild) - heightOfNode(node.rightChild);
     }
 }
